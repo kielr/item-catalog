8687a5c739d8b5fa7f1a2f65b7e7898a68c6d9f5
kielr <regustersk@gmail.com>
2019-02-10 11:09:01 -0800

    added main template as well as navbar. starting login process w/ google

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, render_template
 from database_init import *
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
@@ -7,7 +7,7 @@ import json
 
 
 # Declare app.
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates/')
 
 
 # Connect to the Database.
@@ -17,12 +17,25 @@ Base.metadata.bind = engine
 sql_session = sessionmaker(bind=engine)
 session = sql_session()
 
+
+# Serves the login page.
+@app.route('/login')
+def show_login():
+    return render_template('login.htm')
+
+
+# Serve the front page.
+@app.route('/', methods=['POST', 'GET'])
+def show_home():
+    if request.method == 'GET':
+        return render_template('base.htm')
+
+
 # Creates a new record in the item table.
 @app.route('/items', methods=['POST'])
 def new_item():
     if request.method == 'POST':
         print(request.get_json())
-    
     return make_response(json.dumps("Good!"), 200)
 
 
